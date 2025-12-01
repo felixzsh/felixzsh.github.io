0800dd06ddbeb9b5464584c69e150f7f8ae29fbd
felixzsh <felix.sanchez.dev@gmail.com>
2025-11-30 20:34:16 -0600

    chore: clean comments

--- js/stream.js
+++ js/stream.js
@@ -8,25 +8,20 @@ export class SimpleSyncStream {
     if (typeof data !== 'string') data = String(data);
 
     if (typeof this.destination === 'function') {
-      // Caso TTY: Envía a la función de impresión
       this.destination(data);
     } else if (this.destination instanceof SimpleSyncStream) {
-      // ✅ SOLUCIÓN: Llama al método write() del destino B
       this.destination.write(strData);
     } else {
-      // Caso Captura/Archivo: Acumula
       this.buffer += data;
     }
   }
 
-  // Usado por el comando para leer (ej: context.stdin.read())
   read() {
     const data = this.buffer;
-    this.buffer = ''; // Limpia después de leer
+    this.buffer = '';
     return data;
   }
 
-  // Conecta este stream a otro (para el orquestador de pipes)
   pipe(destStream) {
     this.destination = destStream;
     return destStream;
